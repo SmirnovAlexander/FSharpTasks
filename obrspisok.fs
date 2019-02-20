9976c01621259b44c63daf49b8346912e3eb133f
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-02-20 22:42:42 +0300

    Create obrspisok.fs

---
+++ obrspisok.fs
@@ -0,0 +1,16 @@
+﻿// To make it work faster we will use accumulator argument.
+let rev list =
+    let rec loop list acc=
+        match list with
+        | [] -> acc
+        | head::tail -> loop tail (head::acc)
+    loop list []
+
+// Initialize a list to reverse.
+let somelist = [1..10]
+
+// Get the result.
+let result = rev somelist 
+printf "was: %A, result: %O" somelist result
+
+// Removed readkey because started to use F# interactively.
