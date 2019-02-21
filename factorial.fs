b7592d192a54976f0fb36e6351af8e31382b73c4
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-02-21 19:55:00 +0300

    Raising exeption when input below zero

--- factorial.fs
+++ factorial.fs
@@ -1,8 +1,11 @@
-﻿// Factorial function.
+﻿// Catching input below zero.
+exception WrongInputError of string
+
+// Factorial function.
 let rec factorial n =
     match n with
     | 0 | 1 -> 1
-    | x when x < 0 -> 0
+    | x when x < 0 -> raise (WrongInputError("The given number is below zero."))
     | _ -> n * factorial (n - 1)
 
 // Result.
