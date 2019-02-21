fbca855280393939abf2148fb1930f5a93f8561f
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-02-21 09:03:49 +0300

    Update factorial.fs

--- factorial.fs
+++ factorial.fs
@@ -2,13 +2,9 @@
 let rec factorial n =
     match n with
     | 0 | 1 -> 1
-    | _ -> n * factorial(n-1)
+    | x when x < 0 -> 0
+    | _ -> n * factorial (n - 1)
 
-// Variable to count factorial of.
-let num = 5
-
-// Printing.
-printfn "%d! = %d"  num (factorial (num))
-
-// Freezing console.
-System.Console.ReadKey(true) |> ignore  
+// Result.
+let num = -10
+let result = factorial (num)
