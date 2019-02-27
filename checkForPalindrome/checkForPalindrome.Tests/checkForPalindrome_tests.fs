3e8d66a96c9b5c538d6b5b641dae0351abd3c555
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-02-27 11:49:41 +0300

    Added solution files

---
+++ checkForPalindrome/checkForPalindrome.Tests/checkForPalindrome_tests.fs
@@ -0,0 +1,17 @@
+﻿module tests
+
+open NUnit.Framework
+open FsUnit
+open logic
+
+[<Test>]
+let ``Check if madam is palindrome`` () =
+    checkForPalindrome "madam" |> should equal true
+
+[<Test>]
+let ``Check if "" is palindrome`` () =
+    checkForPalindrome "" |> should equal true
+
+[<Test>]
+let ``Check if math is palindrome`` () =
+    checkForPalindrome "math" |> should equal false
---
+++ checkForPalindrome/checkForPalindrome/checkForPalindrome.fs
@@ -0,0 +1,15 @@
+﻿module logic
+
+// Reversing string using pipe forwars operator.
+let reverse (word: string): string = 
+    word.ToCharArray() |> Array.rev |> System.String
+    
+// Checking for palondrome.
+let checkForPalindrome word =    
+    match word with
+        | reversedWord when reversedWord = reverse word -> true
+        | _ -> false
+    
+// Result.
+let word = "madam"
+let result = checkForPalindrome word
