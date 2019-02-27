253154b470b38099728612cbc77a4636e52b0214
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-02-27 12:10:55 +0300

    Added solution files

---
+++ mergeSort/mergeSort.Tests/mergeSort_tests.fs
@@ -0,0 +1,17 @@
+﻿module tests
+
+open NUnit.Framework
+open FsUnit
+open logic
+
+[<Test>]
+let ``Sorting [7;32;4;6;4;6] should return [4;4;6;6;7;32]`` () =
+    mergeSort [7;32;4;6;4;6] |> should equal [4;4;6;6;7;32]
+
+[<Test>]
+let ``Sorting [] should return []`` () =
+    mergeSort [] |> should equal []
+
+[<Test>]
+let ``Sorting [2] should return [2]`` () =
+    mergeSort [2] |> should equal [2]
