0e8943af467c2052321bf564c8391b199a58804f
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-02-27 10:48:44 +0300

    Added solution files.

---
+++ FindElemInList/FindElemeInList.Tests/findElemInList_tests.fs
@@ -0,0 +1,20 @@
+﻿module tests
+
+
+open NUnit.Framework
+open FsUnit
+open logic
+
+    
+
+[<Test>]
+let ``The index of element "6" in array [7;32;4;6;4;6] is Some(3)`` () =
+    findElemInList [7;32;4;6;4;6] 6 |> should equal (Some(3))
+
+[<Test>]
+let ``The index of element "6" in array [for i in 1..10 -> i*i] is Some(4)`` () =
+    findElemInList [for i in 1..10 -> i*i] 25 |> should equal (Some(4))
+
+[<Test>]
+let ``The index of element "3" in array [] is None`` () =
+    findElemInList [] 3 |> should equal None
