de1ff96a1be02a8fb9d0d577b0e1a2f27f534363
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-03-04 16:24:30 +0300

    Logic added.

--- numberOfEvenNumberInList/numberOfEvenNumberInList/numberOfEvenNumberInList.fs
+++ numberOfEvenNumberInList/numberOfEvenNumberInList/numberOfEvenNumberInList.fs
@@ -1,2 +1,19 @@
 ﻿module logic
 
+    let numberOfEvenNumberInListMap list =
+        let deuces = list |> List.map (fun x -> 
+            match x with
+            | two when two % 2 = 0 -> 2
+            | _ -> 0)
+        
+        let sum = deuces |> List.sum
+        (sum / 2)
+        
+
+    let numberOfEvenNumberInListFilter list =
+        let deuces = list |> List.filter (fun x -> x % 2 = 0)
+        deuces.Length
+
+    let numberOfEvenNumberInListFold list =
+        List.fold (fun acc x -> if (x % 2 = 0) then (acc + 1)
+                                else acc) 0 list
