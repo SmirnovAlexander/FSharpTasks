81b74e925e6d5e0e614ef9da306c48be74417822
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-03-05 09:41:39 +0300

    Added logic
    messed up a little

--- infinitePrimeNumbers/infinitePrimeNumbers/infinitePrimeNumbers.fs
+++ infinitePrimeNumbers/infinitePrimeNumbers/infinitePrimeNumbers.fs
@@ -1,1 +1,11 @@
-﻿module logic
+﻿module logic 
+
+    let isPrime n =
+        match n with
+        | x when x = 0 || x = 1 -> false
+        | _ -> let rec check i =
+                         i > n/2 || (n % i <> 0 && check (i + 1))
+               check 2
+       
+    let primes () = 
+        Seq.initInfinite (fun i -> i) |> Seq.filter (fun x -> isPrime x)
