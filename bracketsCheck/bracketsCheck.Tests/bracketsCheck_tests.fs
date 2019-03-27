2b355631cb73c82eb9bdfcef4da2d1e2a680905e
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-03-27 23:05:22 +0300

    Added logic

--- bracketsCheck/bracketsCheck.Tests/bracketsCheck_tests.fs
+++ bracketsCheck/bracketsCheck.Tests/bracketsCheck_tests.fs
@@ -3,3 +3,23 @@
 open NUnit.Framework
 open FsUnit
 open logic
+
+[<Test>]
+let ``Check for single brackets`` () =
+    bracketsCheck "{x}" |> should equal (Some true)
+
+[<Test>]
+let ``Wrong one`` () =
+    bracketsCheck "(y)(x))" |> should equal (Some false)
+
+[<Test>]
+let ``Wrong order of brackets`` () =
+    bracketsCheck "}[y]y{" |> should equal (Some false)
+
+[<Test>]
+let ``Correct one`` () =
+    bracketsCheck "{}{}sdfsdf()" |> should equal (Some true)
+
+[<Test>]
+let ``Null input`` () =
+    bracketsCheck ""  |> should equal (Some true);
--- bracketsCheck/bracketsCheck/bracketsCheck.fs
+++ bracketsCheck/bracketsCheck/bracketsCheck.fs
@@ -1,1 +1,18 @@
-﻿module logic
+﻿module logic 
+
+    let bracketsCheck (string : string) =
+        let listOfChars = Seq.toList string
+        let rec loop list counter1 counter2 counter3 =
+            match list with
+                | head::tail -> if ((counter1 < 0) || (counter2 < 0) || (counter3 < 0)) then Some(false)
+                                                                                        else match head with 
+                                                                                             | '(' -> loop tail (counter1 + 1) counter2 counter3
+                                                                                             | ')' -> loop tail (counter1 - 1) counter2 counter3
+                                                                                             | '{' -> loop tail counter1 (counter2 + 1) counter3
+                                                                                             | '}' -> loop tail counter1 (counter2 - 1) counter3
+                                                                                             | '[' -> loop tail counter1 counter2 (counter3 + 1)
+                                                                                             | ']' -> loop tail counter1 counter2 (counter3 - 1)
+                                                                                             | _ -> loop tail counter1 counter2 counter3
+                | [] -> if ((counter1 = 0) && (counter2 = 0) && (counter3 = 0)) then Some(true)
+                                                                                else Some(false)
+        loop listOfChars 0 0 0    
