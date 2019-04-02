d9bbe4ca0fd790c61d2e372227ac904d20fc3551
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-04-02 11:35:01 +0300

    Added logic

--- pointFree/PointFree.Tests/pointFreeMap_tests.fs
+++ pointFree/PointFree.Tests/pointFreeMap_tests.fs
@@ -3,4 +3,12 @@
     open NUnit.Framework
     open FsUnit
     open logic
-    open FsCheck.NUnit    
+    open FsCheck.NUnit
+
+    [<Test>]
+    let ``Trivial test`` () =
+        (|>) List.map ( (>>) (*) ) 2 [1;2;3] |> should equal [2;4;6]
+
+    [<Property>]
+    let ``Running tests with FsCheck``(x:int, l:list<int>) =        
+        (|>) List.map ( (>>) (*) ) x l = (List.map (fun y -> y * x) l)
--- pointFree/pointFree/pointFreeMap.fs
+++ pointFree/pointFree/pointFreeMap.fs
@@ -1,1 +1,11 @@
-﻿module logic 
+﻿module logic
+
+    let pointFreeMap x l = List.map (fun y -> y * x) l
+    
+    let pointFreeMap'1 x l = List.map ( (*) x) l
+  
+    let pointFreeMap'2 x = List.map ( (*) x)
+    
+    let pointFreeMap'3 = fun x -> List.map ( (*) x)
+    
+    let pointFreeMap'4 = (|>) List.map ( (>>) (*) )
