3cbcca9a73401799d277c248c54aaac95ee7b1e6
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-04-12 19:06:44 +0300

    Added logic

--- printSquare/printSquare.Tests/printSquare_tests.fs
+++ printSquare/printSquare.Tests/printSquare_tests.fs
@@ -1,1 +1,9 @@
 ﻿module Tests
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic
+
+    [<Test>]
+    let ``Check for 3* square`` () =
+        prepareString 3 |> should equal "***\n* *\n***\n"
--- printSquare/printSquare/printSquare.fs
+++ printSquare/printSquare/printSquare.fs
@@ -1,1 +1,32 @@
-﻿module Logic
+﻿module Logic 
+
+    open System
+
+    // Get string of square with spaces in it.
+    let rec stringWithSpace index (str : string) =
+        match index with
+            |2 -> "*" + str + "*" + "\n"
+            |_ -> stringWithSpace (index - 1) (str.Insert(0," "))
+
+    // Get string of square without spaces in it.
+    let rec stringWithoutSpace index (str : string) =
+            match index with
+            |1 -> str + "*" + "\n"
+            |_ -> stringWithoutSpace (index - 1) (str.Insert(0,"*"))
+
+    // Preparing our string.
+    let prepareString n =
+        let rec loop counter accString=              
+            match counter with
+                |1 -> [accString; stringWithoutSpace n  ""] |> String.concat ""
+                |_ when counter = n ->                
+                    let accStringLocal =  [accString; stringWithoutSpace n ""] |> String.concat ""
+                    loop (counter - 1) accStringLocal
+                |_ ->
+                    let accStringLocal =  [accString; stringWithSpace n ""] |> String.concat ""
+                    loop (counter - 1) accStringLocal
+        loop n ""
+     
+    // Printing string.
+    let printSquare (str: string) = 
+        Console.WriteLine(str)
