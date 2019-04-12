e8ef4573e169b481b5ed4d318a7cd90c4c3cf8a3
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-04-12 16:54:46 +0300

    Styleguided

--- FindElemInList/FindElemInList/FindElemInList.fs
+++ FindElemInList/FindElemInList/FindElemInList.fs
@@ -1,16 +1,16 @@
-﻿module logic
+﻿module Logic
 
-// Find index of first element appearence in the list.
-let findElemInList list elementToFind =
-    let rec loop list elementToFind counter=
-        match list with
-        | head :: tail -> if (head = elementToFind)
-                            then Some(counter)
-                            else loop tail elementToFind (counter + 1)
-        | [] -> None
-    loop list elementToFind 0
+    // Find index of first element appearence in the list.
+    let findElemInList list elementToFind =
+        let rec loop list elementToFind counter =
+            match list with
+            | head :: tail -> if (head = elementToFind)
+                                then Some(counter)
+                                else loop tail elementToFind (counter + 1)
+            | [] -> None
+        loop list elementToFind 0
              
-// Result.
-let list = [7;32;4;6;4;6]
-let elementToFind = 6
-let result = findElemInList list elementToFind
+    // Result.
+    let list = [7; 32; 4; 6; 4; 6]
+    let elementToFind = 6
+    let result = findElemInList list elementToFind
