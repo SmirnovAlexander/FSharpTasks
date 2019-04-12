5765e4bb4b699cd339a6ce26259d083535b0db69
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-04-12 17:04:04 +0300

    Styleguided

--- mergeSort/mergeSort.Tests/mergeSort_tests.fs
+++ mergeSort/mergeSort.Tests/mergeSort_tests.fs
@@ -1,17 +1,17 @@
-﻿module tests
+﻿module Tests
 
-open NUnit.Framework
-open FsUnit
-open logic
+    open NUnit.Framework
+    open FsUnit
+    open Logic
 
-[<Test>]
-let ``Sorting [7;32;4;6;4;6] should return [4;4;6;6;7;32]`` () =
-    mergeSort [7;32;4;6;4;6] |> should equal [4;4;6;6;7;32]
+    [<Test>]
+    let ``Sorting [7; 32; 4; 6; 4; 6] should return [4; 4; 6; 6; 7; 32]`` () =
+        mergeSort [7; 32; 4; 6; 4; 6] |> should equal [4; 4; 6 ;6; 7; 32]
 
-[<Test>]
-let ``Sorting [] should return []`` () =
-    mergeSort [] |> should equal []
+    [<Test>]
+    let ``Sorting [] should return []`` () =
+        mergeSort [] |> should equal []
 
-[<Test>]
-let ``Sorting [2] should return [2]`` () =
-    mergeSort [2] |> should equal [2]
+    [<Test>]
+    let ``Sorting [2] should return [2]`` () =
+        mergeSort [2] |> should equal [2]
--- mergeSort/mergeSort/mergeSort.fs
+++ mergeSort/mergeSort/mergeSort.fs
@@ -1,36 +1,32 @@
-﻿module logic
+﻿module Logic
 
-// Returnes 2 lists cutted from 1 list in the center.
-let cutTheList list = 
-    let rec loop (list: 'a list) (rightList: 'a list) (leftList:'a list) : ('a list * 'a list) = 
-        match list with
-        | [] -> (rightList,leftList)
-        | [x] -> (x::rightList,leftList)
-        | firstElement::secondElement::tail -> loop tail (firstElement::rightList) (secondElement::leftList)
-    loop list [] []
+    // Returnes 2 lists cutted from 1 list in the center.
+    let cutTheList list = 
+        let rec loop (list: 'a list) (rightList: 'a list) (leftList:'a list) : ('a list * 'a list) = 
+            match list with
+            | [] -> (rightList, leftList)
+            | [x] -> (x::rightList, leftList)
+            | firstElement::secondElement::tail -> loop tail (firstElement::rightList) (secondElement::leftList)
+        loop list [] []
 
-// Merges two sorted lists in one sorted list. 
-let rec merge (list1: 'a list) (list2:'a list) : 'a list =
-    match list1, list2 with
-    | [], list -> list
-    | list, [] -> list
-    | firstElemInFirstList::tailOfFirstList, firstElemInSecondList::tailfOfSecondList when 
-        firstElemInFirstList < firstElemInSecondList -> 
-            firstElemInFirstList::(merge tailOfFirstList (firstElemInSecondList::tailfOfSecondList))
-    | firstList, firstElemInSecondList::tailfOfSecondList -> 
-        firstElemInSecondList::(merge firstList tailfOfSecondList)
+    // Merges two sorted lists in one sorted list. 
+    let rec merge (list1: 'a list) (list2:'a list) : 'a list =
+        match list1, list2 with
+        | [], list -> list
+        | list, [] -> list
+        | firstElemInFirstList::tailOfFirstList, firstElemInSecondList::tailfOfSecondList when 
+            firstElemInFirstList < firstElemInSecondList -> 
+                firstElemInFirstList::(merge tailOfFirstList (firstElemInSecondList::tailfOfSecondList))
+        | firstList, firstElemInSecondList::tailfOfSecondList -> 
+            firstElemInSecondList::(merge firstList tailfOfSecondList)
 
-// Merge sort algorythm.
-let rec mergeSort (listToSort:'a list) : 'a list =
-    match listToSort with
-    | [] -> []
-    | [x] -> [x]
-    | list ->
-        let (firstHalf,secondHalf) = cutTheList(list)
-        let firstHalfSorted = mergeSort(firstHalf)
-        let secondHalfSorted = mergeSort(secondHalf)
-        merge firstHalfSorted secondHalfSorted
-
-// Result.
-let list = [5;3;2;7;8]
-let result = mergeSort list
+    // Merge sort algorythm.
+    let rec mergeSort (listToSort:'a list) : 'a list =
+        match listToSort with
+        | [] -> []
+        | [x] -> [x]
+        | list ->
+            let (firstHalf,secondHalf) = cutTheList(list)
+            let firstHalfSorted = mergeSort(firstHalf)
+            let secondHalfSorted = mergeSort(secondHalf)
+            merge firstHalfSorted secondHalfSorted
