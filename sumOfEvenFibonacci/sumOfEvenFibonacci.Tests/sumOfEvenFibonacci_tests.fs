70dcaf0f7860f5c84c3aee4b51a004cb5fdfa4dd
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-04-12 17:58:09 +0300

    Added logic

--- sumOfEvenFibonacci/sumOfEvenFibonacci.Tests/sumOfEvenFibonacci_tests.fs
+++ sumOfEvenFibonacci/sumOfEvenFibonacci.Tests/sumOfEvenFibonacci_tests.fs
@@ -1,1 +1,9 @@
 ﻿module Tests
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic
+
+    [<Test>]
+    let ``Sum of all even fibonacci elements below 1000000 should be 1089154`` () =
+        sumOfEvenFibonacciLessThenMillion ()|> should equal 1089154
--- sumOfEvenFibonacci/sumOfEvenFibonacci/sumOfEvenFibonacci.fs
+++ sumOfEvenFibonacci/sumOfEvenFibonacci/sumOfEvenFibonacci.fs
@@ -1,1 +1,18 @@
-﻿module Logic
+﻿module Logic 
+
+    // Fibonacci recursion function with extra pointer.
+    let sumOfEvenFibonacciLessThenMillion () =
+        let rec loop counter prev2 prev1 summary =
+            match counter with
+            | _ ->
+                let decreasedCounter = counter - 1
+                let sum = prev2 + prev1
+                if ( prev2 < 1000000 ) then
+                    if ( (prev2 % 2) = 0 ) then
+                        let summaryIncreased = summary + prev2
+                        loop decreasedCounter prev1 sum summaryIncreased                    
+                    else 
+                        loop decreasedCounter prev1 sum summary
+                else summary
+                
+        loop 123456 0 1 0
