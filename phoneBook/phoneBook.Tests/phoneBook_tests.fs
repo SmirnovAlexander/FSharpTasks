34005a5b9593d094e4cc5d370da0ae1bdb803012
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-04-17 01:57:49 +0300

    Added logic

--- phoneBook/phoneBook.Tests/phoneBook_tests.fs
+++ phoneBook/phoneBook.Tests/phoneBook_tests.fs
@@ -3,3 +3,88 @@
     open NUnit.Framework
     open FsUnit
     open Logic    
+    
+    let book = phoneBook ()
+
+    (*
+        Add record tests.
+    *)
+
+    // Map with 3 elements to
+    // compare our adding with.
+    let mapToCompare = Map.empty
+                              .Add("Sanya","+79119727982")
+                              .Add("Fedos","+79312208944")
+                              .Add("Taya","+79210938070")
+
+    [<Test>]
+    [<Order(1)>]
+    let ``Adding some elements to fill book.``() =
+
+        book.addToBook("Sanya","+79119727982") |> ignore
+        book.addToBook("Fedos","+79312208944") |> ignore
+        book.addToBook("Taya","+79210938070") |> ignore
+                            
+        book.book |> should equal mapToCompare
+
+    // Adding an element to our comparing map.
+    let mapToCompare'1 = mapToCompare
+                              .Add("Ilya","+79633289584")
+    
+    [<Test>]
+    [<Order(2)>]
+    let ``Adding another element.``() =
+
+        book.addToBook("Ilya","+79633289584") |> ignore
+              
+        book.book |> should equal mapToCompare'1
+
+    [<Test>]
+    [<Order(3)>]
+    let ``Adding element with a name that already been in DB. Element should not be added.``() =
+
+        book.addToBook("Sanya","88005553535") |> ignore
+                            
+        book.book |> should equal mapToCompare'1
+
+    [<Test>]
+    [<Order(4)>]
+    let ``Adding element with a phone that already been in DB. Element should not be added.``() =
+
+        book.addToBook("Egor","+79119727982") |> ignore
+                            
+        book.book |> should equal mapToCompare'1
+
+    (*
+        Find name and phone tests.
+    *)
+
+    [<Test>]
+    [<Order(5)>]
+    let ``Search for a Sanya number.``() =                            
+        book.findPhoneByName "Sanya" |> should equal "+79119727982"
+
+    [<Test>]
+    [<Order(6)>]
+    let ``Search for a number of a person who is not in DB.``() =                            
+        book.findPhoneByName "Vaselisa" |> should equal "No phone matching this name."
+
+    [<Test>]
+    [<Order(7)>]
+    let ``Searching for a name matching +79312208944 phone.``() =                            
+        book.findNameByPhone "+79312208944" |> should equal "Fedos"
+    
+    [<Test>]
+    [<Order(8)>]
+    let ``Search for a name by phone which is not in DB.``() =                            
+        book.findNameByPhone "88005553535" |> should equal "No name matching this phone."
+
+    (*
+        Test shows that database serealized and deserilized correctly.
+    *)
+
+    [<Test>]
+    [<Order(9)>]
+    let ``Comparing DB to its decerialized serealized version.``() = 
+        book.serealize
+        book.book |> should equal book.deserealize
--- phoneBook/phoneBook/phoneBook.fs
+++ phoneBook/phoneBook/phoneBook.fs
@@ -1,1 +1,170 @@
 ﻿module Logic
+
+    open System
+    open System.IO
+    open System.Runtime.Serialization.Formatters.Binary
+    
+    // Our phone book type.
+    type phoneBook () = 
+
+        (*
+            Book member.         
+        *)
+
+        // Phone book.
+        let mutable mutBook =
+            Map.empty
+
+        // Class member phone book.
+        member this.book = mutBook
+
+         (*
+             Methods to deal with a book.
+         *)
+
+        // Printing book.
+        member this.printBook = 
+            for record in this.book do
+                printfn "%s, %s" record.Key record.Value
+
+        // Add record to a book.
+        // Addind record only with unique name and phone.
+        member this.addToBook(name: string, phone: string) =
+            
+            let mutable counter = 0
+            
+            for record in this.book do
+                if (record.Key = name || record.Value = phone) then
+                    counter <- counter + 1
+
+            if (counter = 0) then
+                mutBook <- this.book.Add(name, phone)
+                true 
+                             else
+                false
+
+        // Find phone by name.
+        member this.findPhoneByName (name: string) =
+            
+            let mutable ifExists = false
+            let mutable phone = ""
+            
+            for record in this.book do
+                if (record.Key = name) then
+                    phone <- record.Value
+                    ifExists <- true
+            
+            if (ifExists) then phone
+                          else "No phone matching this name."
+        
+        // Find name by phone.
+        member this.findNameByPhone (phone: string) =
+            
+            let mutable ifExists = false
+            let mutable name = ""
+            
+            for record in this.book do
+                if (record.Value = phone) then
+                    name <- record.Key
+                    ifExists <- true
+            
+            if (ifExists) then name
+                          else "No name matching this phone."
+
+        // Serializing our DB in file.
+        member this.serealize =
+            
+            // Method to write values into file.
+            let writeValue outputStream (x: 'a) =
+                let formatter = new BinaryFormatter()
+                formatter.Serialize(outputStream, box x)
+
+            let fsOut = new FileStream("Data.dat", FileMode.Create)
+            writeValue fsOut this.book
+            fsOut.Close()
+
+        // Deserializing our DB from file.
+        member this.deserealize =
+            
+            // Method to read values from file.
+            let readValue inputStream =
+                let formatter = new BinaryFormatter()
+                let res = formatter.Deserialize(inputStream)
+                unbox res
+
+            // Trying to find file.
+            try
+                let fsIn = new FileStream("Data.dat", FileMode.Open)
+                let res : Map<string, string> = readValue fsIn            
+                fsIn.Close()
+                printfn "Successfully loaded data!"
+                res
+            with
+                | :? System.IO.FileNotFoundException -> printfn "Had not found file!"; this.book
+            
+
+        (*
+            Phone book console cycle.
+        *)
+
+        // Method to print available options.
+        member this.printOptinons = 
+            printf "----------\n1. Exit\n2. Add record (name and phone)\n3. Find phone by name\n4. Find name by phone\n5. Show database\n6. Save database in file\n7. Load database from file\n----------\n"
+
+        // Method starting the loop. 
+        member this.startBook =
+            
+            // Exit condition.
+            let mutable exit = false
+
+            // While we not put 1 we run.
+            while (exit = false) do
+
+                // Print available options.
+                this.printOptinons
+
+                // Waiting input
+                let input = Console.ReadLine()
+
+                (*
+                    Handling input.
+                *)
+
+                if (input = "1") then
+                    exit <- true
+
+                else if (input = "2") then
+                    printfn "Enter name:"
+                    let name = Console.ReadLine()
+                    printfn "Enter phone:"
+                    let phone = Console.ReadLine()
+                    if (this.addToBook(name, phone)) then printfn "Succesfully added to book!"
+                                                     else printfn "There is already a record with this name or phone."
+                                      
+                else if (input = "3") then
+                    printfn "Enter a name to search phone:"
+                    let name = Console.ReadLine()
+                    printfn "%s" (this.findPhoneByName name)
+
+                else if (input = "4") then
+                    printfn "Enter a phone to search name:"
+                    let phone = Console.ReadLine()
+                    printfn "%s" (this.findNameByPhone phone)
+
+                else if (input = "5") then
+                    this.printBook
+
+                else if (input = "6") then
+                    this.serealize
+
+                else if (input = "7") then
+                    mutBook <- this.deserealize
+
+                else printfn "Wrong command."
+
+            // Exiting console if exited loop.
+            Environment.Exit(0)
+    
+    
+    let book = phoneBook ()
+    book.startBook
