aaf99fbd86074fb418989540c7a7dd3fef5ee5e8
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-04-18 19:37:24 +0300

    Added logic

--- workflowCalculatingStrings/workflowCalculatingStrings/CalculatingStrings.fs
+++ workflowCalculatingStrings/workflowCalculatingStrings/CalculatingStrings.fs
@@ -1,1 +1,10 @@
 ﻿module Logic
+    
+    type StringCalculationBuilder() =
+        member this.Bind(x: string, f) = 
+            try
+                f (int x)
+            with
+                | :? System.FormatException -> printfn "Wrong input type!"; None
+        member this.Return(x) =
+            Some(x)
