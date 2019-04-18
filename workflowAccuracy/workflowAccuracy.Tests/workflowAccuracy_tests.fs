4071a2ed5051a291c5f64fbb1b7f36aea1376784
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-04-18 18:06:54 +0300

    Added logic

--- workflowAccuracy/workflowAccuracy.Tests/workflowAccuracy_tests.fs
+++ workflowAccuracy/workflowAccuracy.Tests/workflowAccuracy_tests.fs
@@ -1,3 +1,32 @@
 ﻿module Tests
 
     open Logic
+    open NUnit.Framework
+    open FsUnit
+
+    [<Test>]
+    let ``3 symbols after dot``() =
+        let result = RoundBuilder 3 {
+            let! a = 2.0 / 12.0
+            let! b = 3.5
+            return a / b
+        }
+        result |> should equal 0.048
+
+    [<Test>]
+    let ``5 symbols after dot``() =
+        let result = RoundBuilder 5 {
+            let! a = 8.0
+            let! b = 13.0
+            return a / b
+        }
+        result |> should equal 0.61538
+
+    [<Test>]
+    let ``7 symbols after dot``() =
+        let result = RoundBuilder 7 {
+            let! a = 8.0
+            let! b = 7.0
+            return a / b
+        }
+        result |> should equal 1.1428571
--- workflowAccuracy/workflowAccuracy/workflowAccuracy.fs
+++ workflowAccuracy/workflowAccuracy/workflowAccuracy.fs
@@ -1,1 +1,9 @@
 ﻿module Logic
+
+    open System
+
+    type RoundBuilder(accuracy: int) =
+        member this.Bind(x, f) =
+            f x
+        member this.Return(x: float) = 
+            Math.Round(x, accuracy)
