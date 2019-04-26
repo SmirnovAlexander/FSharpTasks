daa8f6ba7d9264c44895c50297f49df2cbe53873
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-04-26 19:00:37 +0300

    Added logic

---
+++ BiggestPolynome/BiggestPolynome.Tests/BiggestPalindrome_tests.fs
@@ -0,0 +1,17 @@
+﻿module Tests
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic
+
+    [<Test>]
+    let ``Check palindrome function works correctly (1)`` () =
+        checkForPalindrome 1001 |> should equal true
+
+    [<Test>]
+    let ``Check palindrome function works correctly (2)`` () =
+        checkForPalindrome 1021 |> should equal false
+
+    [<Test>]
+    let ``Final check`` () =
+        BiggestPalindrome () |> should equal 906609
---
+++ BiggestPolynome/BiggestPolynome/BiggestPalindrome.fs
@@ -0,0 +1,24 @@
+﻿module Logic 
+
+    // Method that reverses number
+    let reverse (n: int) =
+        let rec loop (n: int, partial: int) =
+            if (n = 0) then partial
+                       else loop(n / 10, partial * 10 + n % 10)
+        loop (n, 0)
+
+    // Checking number if it is palondrome.
+    let checkForPalindrome number =    
+        number = reverse number     
+
+    // Iterating throw each pair of three digit numbers.
+    let BiggestPalindrome () = 
+        let rec loop (first:int, second:int, maxValue: int) = 
+            match first with
+            | 99 -> maxValue
+            | _ when checkForPalindrome (first * second) ->
+                if (first * second > maxValue) then loop (first, (second - 1), (first * second))
+                                               else loop (first, (second - 1), maxValue)
+            | _ when (second = 100) -> loop ((first - 1), 999, maxValue)
+            | _ -> loop (first, (second - 1), maxValue)
+        loop (999, 999, 0)
