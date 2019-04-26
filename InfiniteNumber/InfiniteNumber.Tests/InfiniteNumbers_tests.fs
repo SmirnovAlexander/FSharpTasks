40906a12773c4416be1a6f733e92f9d6df0a913c
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-04-26 18:01:33 +0300

    Added logic

--- InfiniteNumber/InfiniteNumber.Tests/InfiniteNumbers_tests.fs
+++ InfiniteNumber/InfiniteNumber.Tests/InfiniteNumbers_tests.fs
@@ -1,1 +1,17 @@
 ﻿module Tests
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic
+
+    [<Test>]
+    let ``First 4 numbers of first sequence are [1; -1; 1; -1]`` () =
+        ones () |> Seq.take 4 |> Seq.toArray |> should equal [1; -1; 1; -1]
+
+    [<Test>]
+    let ``First 4 numbers of second sequence are [1; 2; 3; 4]`` () =
+        numbers () |> Seq.take 4 |> Seq.toArray |> should equal [1; 2; 3; 4]
+    
+    [<Test>]
+    let ``First 4 numbers of final sequence are [1, -2, 3, -4]`` () =
+        series () |> Seq.take 4 |> Seq.toArray |> should equal [1; -2; 3; -4]
--- InfiniteNumber/InfiniteNumber/InfiniteNumbers.fs
+++ InfiniteNumber/InfiniteNumber/InfiniteNumbers.fs
@@ -1,1 +1,10 @@
 ﻿module Logic
+
+    let ones () = 
+        Seq.initInfinite (fun x -> pown (-1) x)
+
+    let numbers () =
+        Seq.initInfinite (fun x -> x + 1)
+
+    let series () =
+        Seq.map2 (fun x y -> x * y) (ones ()) (numbers ())
