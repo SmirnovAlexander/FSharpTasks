82114f680a4f4338deac23ec0ca30f5b9a0eb6af
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-05-10 20:00:30 +0300

    Added logic

--- HashTable/HashTable.Tests/HashTableClassTests.fs
+++ HashTable/HashTable.Tests/HashTableClassTests.fs
@@ -1,1 +1,20 @@
 ﻿module Tests
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic 
+    open System
+
+    let comparator = StringComparer.CurrentCulture
+    let hashTable = HashMap(comparator)
+
+    [<Test>]
+    let ``Add and count`` () =
+        hashTable.Add("Bob","Msk")
+        hashTable.Add("Mark","Spb")
+        hashTable.Count |> should equal 2
+
+    [<Test>]
+    let ``Remove`` () =
+        hashTable.Remove("Mark")
+        hashTable.Count |> should equal 1
