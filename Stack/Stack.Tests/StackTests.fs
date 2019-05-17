f8b13f73a82dacaccfe646b3df882e4bcecad216
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-05-17 20:43:43 +0300

    Added logic

--- Stack/Stack.Tests/StackTests.fs
+++ Stack/Stack.Tests/StackTests.fs
@@ -1,1 +1,37 @@
 ﻿module Tests
+
+    open NUnit.Framework
+    open FsUnit
+    open Logic 
+
+    [<Test>]
+    let ``Push`` () =
+        let stack = Stack()
+        stack.Push(1)
+        stack.Push(2)
+        stack.Push(3)
+        stack.GetStack() |> should equal [3; 2; 1]
+    
+    [<Test>]
+    let ``Pop`` () =
+        let stack = Stack()
+        stack.Push(1)
+        stack.Push(2)
+        stack.Push(3)
+        stack.Pop |> should equal (3, [2; 1])
+
+    [<Test>]
+    let ``Pop from empty stack`` () =
+        let stack = Stack()
+        (fun () -> stack.Pop |> ignore) |> should throw typeof<System.Exception>
+
+    [<Test>]
+    let ``IsEmpty (true)`` () =
+        let stack = Stack()
+        stack.IsEmpty |> should equal true
+
+    [<Test>]
+    let ``IsEmpty (false)`` () =
+        let stack = Stack()
+        stack.Push(1)
+        stack.IsEmpty |> should equal false
--- Stack/Stack/Stack.fs
+++ Stack/Stack/Stack.fs
@@ -1,1 +1,33 @@
 ﻿module Logic
+
+    // Stack type.
+    type Stack() = 
+
+        // Private mutable list.
+        let mutable stack = []
+        
+        // Pushing one element.
+        member this.Push element =
+            stack <- element::stack
+
+        // Popping element.
+        // If there are elements in stack we return
+        // top element and rest of a stack.
+        member this.Pop = 
+            match stack with 
+            | top::rest -> 
+                stack <- rest
+                (top, stack)
+            | [] -> failwith "Stack underflow"
+        
+        // If stack is empty.
+        member this.IsEmpty = 
+            match stack with 
+            | [] -> 
+                true
+            | _ -> 
+                false
+
+        // Getting stack.
+        member this.GetStack() =
+            stack
