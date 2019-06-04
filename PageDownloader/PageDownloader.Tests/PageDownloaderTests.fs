33c078164d83b7b77c08a966275dbea6b1c35c8a
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-06-04 15:11:09 +0300

    Added logic

--- PageDownloader/PageDownloader.Tests/PageDownloaderTests.fs
+++ PageDownloader/PageDownloader.Tests/PageDownloaderTests.fs
@@ -1,1 +1,9 @@
 ﻿module Tests
+
+    open Logic
+    open NUnit.Framework
+    open FsUnit
+
+    [<Test>]
+    let ``NumberOfLettersOnInnerPages``() =
+        NumberOfLettersOnInnerPages("http://hwproj.me/courses/31") |> should equal null
--- PageDownloader/PageDownloader/PageDownloader.fs
+++ PageDownloader/PageDownloader/PageDownloader.fs
@@ -1,1 +1,36 @@
 ﻿module Logic
+   
+    open FSharp.Data
+    open System.Net
+    open System.IO
+
+    // Gets web page, downloads all pages that have their links on input page
+    // and prints information about every page in format "url --- number of symbols".
+    let NumberOfLettersOnInnerPages (page: string) =
+    
+        // Getting html of current page.
+        let html = HtmlDocument.Load(page)
+
+        // Filtering html to get links.
+        let links = 
+            html.Descendants ["a"]
+            |> Seq.choose (fun x -> 
+                   x.TryGetAttribute("href")
+                   |> Option.map (fun a -> a.Value()))
+            |> Seq.filter (fun x -> 
+                        x.StartsWith("http"))
+            |> Seq.toList
+       
+         // Asynchroniously getting page content and printing information.        
+        let fetchAsync (url: string) =
+            async {                
+                let request = WebRequest.Create(url)
+                use! response = request.AsyncGetResponse()
+                use stream = response.GetResponseStream()
+                use reader = new StreamReader(stream)
+                let html = reader.ReadToEnd()
+                do printfn "Read %d characters for %s..." html.Length url
+            }
+       
+        // Executing fetchAsync method for every link.
+        links |> List.map (fun site -> site |> fetchAsync |> Async.Start) |> ignore
