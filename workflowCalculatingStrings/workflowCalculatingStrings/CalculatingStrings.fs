7a44958268816d3aeb3b293ae47bb40c2ed2c660
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-06-09 22:07:13 +0300

    Added Int32.TryParse instead of try with

--- workflowCalculatingStrings/workflowCalculatingStrings/CalculatingStrings.fs
+++ workflowCalculatingStrings/workflowCalculatingStrings/CalculatingStrings.fs
@@ -1,10 +1,13 @@
 ﻿module Logic
     
+    open System
+
+    // Workflow that make math operations with numbers
+    // given in strings.
     type StringCalculationBuilder() =
-        member this.Bind(x: string, f) = 
-            try
-                f (int x)
-            with
-                | :? System.FormatException -> printfn "Wrong input type!"; None
+        member this.Bind(x: string, f) =                         
+            let res = Int32.TryParse(x)
+            if  (fst res) then f (snd res)
+            else None                             
         member this.Return(x) =
             Some(x)
