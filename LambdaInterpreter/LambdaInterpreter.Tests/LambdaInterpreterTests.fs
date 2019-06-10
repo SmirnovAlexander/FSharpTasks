531d9e542d5c2c490d4b275f3b1f850065fe7dfc
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-06-10 17:08:28 +0300

    Added test

--- LambdaInterpreter/LambdaInterpreter.Tests/LambdaInterpreterTests.fs
+++ LambdaInterpreter/LambdaInterpreter.Tests/LambdaInterpreterTests.fs
@@ -10,16 +10,20 @@
 
     [<Test>]
     let ``(\x.x \y.y) -----> \y.y``() =        
-        (interpString("(\\x.x \\y.y)")) |> should equal ("\\y.y")
+        (interpString(@"(\x.x \y.y)")) |> should equal ("\\y.y")
 
     [<Test>]
     let ``(\x.x -----> \x.x)``() =        
-        (interpString("\\x.x")) |> should equal ("\\x.x")
+        (interpString(@"\x.x")) |> should equal ("\\x.x")
 
     [<Test>]
     let ``((\x.x \y.y) \z.(z z)) -----> \z.(z z)``() =        
-        (interpString("((\\x.x \\y.y)\\z.(z z))")) |> should equal ("\\z.(z z)")
+        (interpString(@"((\x.x \y.y)\z.(z z))")) |> should equal ("\\z.(z z)")
 
     [<Test>]
     let ``((\x.\y.\z.(z z) \y.y) -----> \y.\z.(z z))``() =        
-        (interpString("(\\x.\\y.\\z.(z z) \\y.y)")) |> should equal ("\\y.\\z.(z z)")                
+        (interpString(@"(\x.\y.\z.(z z) \y.y)")) |> should equal ("\\y.\\z.(z z)")  
+        
+    [<Test>]
+    let capture() =
+        (interpString(@"((\x.\y.(x y) \t.y) \b.b)")) |> should equal ("\\b.b")     
