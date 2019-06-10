487bf6969f9dc4da7244c81a30cb3cd548bf3d53
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-06-10 17:26:11 +0300

    Added some tests

--- Lazy/Lazy.Tests/LazyTests.fs
+++ Lazy/Lazy.Tests/LazyTests.fs
@@ -13,11 +13,26 @@
         (lazyObj :> ILazy<int>).Get() |> should equal 1
 
     [<Test>]
+    let ``CreateSingleThreadedLazy(calculating value once)``() =
+        let mutable counter = (int64) 0
+        let lazyObj = LazyFactory.CreateSingleThreadedLazy(fun () -> 
+            Interlocked.Increment &counter |> ignore 
+            (Interlocked.Read &counter) |> should equal 1)             
+        for i in 1..10 do
+            Task.Run(fun () -> (lazyObj :> ILazy<unit>).Get()) |> ignore
+
+    [<Test>]
+    let ``CreateMultiThreadedLazy(all values match)``() =
+        let lazyObj = LazyFactory.CreateMultiThreadedLazy(fun () -> new obj())
+        let expression = (lazyObj :> ILazy<obj>).Get()
+        Seq.map(fun obj -> expression |> ((lazyObj :> ILazy<obj>).Get ()).Equals |> should be True) [|1..10|] |> ignore
+
+    [<Test>]
     let ``CreateMultiThreadedLazy(calculating value once)``() =
-        let mutable count = (int64) 0
+        let mutable counter = (int64) 0
         let lazyObj = LazyFactory.CreateMultiThreadedLazy(fun () -> 
-            Interlocked.Increment &count |> ignore 
-            (Interlocked.Read &count) |> should equal 1)             
+            Interlocked.Increment &counter |> ignore 
+            (Interlocked.Read &counter) |> should equal 1)             
         for i in 1..10 do
             Task.Run(fun () -> (lazyObj :> ILazy<unit>).Get()) |> ignore
 
@@ -25,5 +40,20 @@
     let ``CreateMultiLockfreeThreadedLazy``() =
         let lazyObj = LazyFactory.CreateMultiLockfreeThreadedLazy(fun () -> 1)
         (lazyObj :> ILazy<int>).Get () |> should equal 1
+
+    [<Test>]
+    let ``CreateMultiLockfreeThreadedLazy(all values match)``() =
+        let lazyObj = LazyFactory.CreateMultiLockfreeThreadedLazy(fun () -> new obj())
+        let expression = (lazyObj :> ILazy<obj>).Get()
+        Seq.map(fun obj -> expression |> ((lazyObj :> ILazy<obj>).Get ()).Equals |> should be True) [|1..10|] |> ignore
+
+    [<Test>]
+    let ``CreateMultiLockfreeThreadedLazy(calculating value once)``() =
+        let mutable counter = (int64) 0
+        let lazyObj = LazyFactory.CreateMultiLockfreeThreadedLazy(fun () -> 
+            Interlocked.Increment &counter |> ignore 
+            (Interlocked.Read &counter) |> should equal 1)             
+        for i in 1..10 do
+            Task.Run(fun () -> (lazyObj :> ILazy<unit>).Get()) |> ignore    
 
     
