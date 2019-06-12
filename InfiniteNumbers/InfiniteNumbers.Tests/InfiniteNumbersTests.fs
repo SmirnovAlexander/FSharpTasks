a5db7d05362f5b24bbb56273da503fe225076cc6
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-06-12 17:13:12 +0300

    Renamed files and added comments

--- InfiniteNumbers/InfiniteNumbers.Tests/InfiniteNumbersTests.fs
+++ InfiniteNumbers/InfiniteNumbers.Tests/InfiniteNumbersTests.fs
@@ -6,12 +6,12 @@
 
     [<Test>]
     let ``First 4 numbers of first sequence are [1; -1; 1; -1]`` () =
-        ones () |> Seq.take 4 |> Seq.toArray |> should equal [1; -1; 1; -1]
+        ones () |> Seq.take 4 |> Seq.toList |> should equal [1; -1; 1; -1]
 
     [<Test>]
     let ``First 4 numbers of second sequence are [1; 2; 3; 4]`` () =
-        numbers () |> Seq.take 4 |> Seq.toArray |> should equal [1; 2; 3; 4]
+        numbers () |> Seq.take 4 |> Seq.toList |> should equal [1; 2; 3; 4]
     
     [<Test>]
     let ``First 4 numbers of final sequence are [1, -2, 3, -4]`` () =
-        series () |> Seq.take 4 |> Seq.toArray |> should equal [1; -2; 3; -4]
+        series () |> Seq.take 4 |> Seq.toList |> should equal [1; -2; 3; -4]
--- InfiniteNumbers/InfiniteNumbers/InfiniteNumbers.fs
+++ InfiniteNumbers/InfiniteNumbers/InfiniteNumbers.fs
@@ -1,10 +1,13 @@
 ﻿module Logic
 
+    // Infinite sequence of [1; -1; 1; -1 ...]
     let ones () = 
         Seq.initInfinite (fun x -> pown (-1) x)
 
+    // Infinite sequence of [1; 2; 3; 4 ...]
     let numbers () =
         Seq.initInfinite (fun x -> x + 1)
 
+    // Infinite sequence of [1, -2, 3, -4, 5, -6, ...]
     let series () =
         Seq.map2 (fun x y -> x * y) (ones ()) (numbers ())
