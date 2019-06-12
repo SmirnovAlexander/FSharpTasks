f66ecea67cf2eb285602e7a5f2176f9aa33899a0
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-06-12 15:51:52 +0300

    Uploaded project back

---
+++ InfinitePrimeNumbers/InfinitePrimeNumbers/InfinitePrimeNumbers.fs
@@ -0,0 +1,12 @@
+﻿module Logic 
+    
+    // Check if number is prime.
+    let isPrime n =
+        match n with
+            | 0 | 1 -> false
+            | _ -> let rec check i =
+                             (i > int (sqrt (float n))) || (n % i <> 0 && check (i + 1))
+                   check 2
+    // Initializing infinite sequence.
+    let primes () = 
+        Seq.initInfinite (id) |> Seq.filter (isPrime)
