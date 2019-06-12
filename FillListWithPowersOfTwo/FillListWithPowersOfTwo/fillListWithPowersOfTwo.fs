1fec2a6a955b1e63102441cad5cb46bfd7a1310f
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-06-12 14:43:32 +0300

    Added tests and appveyor.yml

---
+++ FillListWithPowersOfTwo/FillListWithPowersOfTwo/fillListWithPowersOfTwo.fs
@@ -0,0 +1,27 @@
+﻿module Logic
+
+    // Exponentiation.
+    let rec power number pow =
+        if pow = 0 then 1
+        else if pow = 1 then number
+        else number * power number (pow - 1)
+
+    // List reverse.
+    let reverse list =
+        let rec loop list acc=
+            match list with
+            | [] -> acc
+            | head::tail -> loop tail (head::acc)
+        loop list []
+
+    // Returns list filled with powers of 2.
+    let fillListWithPowersOfTwo powerToStart numberOfElements =
+        let numberOfElementsMinusOne = numberOfElements - 1
+        let rec loop powerToStart numberOfElementsMinusOne (list:list<int>) acc=
+            match list.Length with
+            | i when i = numberOfElementsMinusOne + 1 -> reverse list
+            | 0 -> let p = power 2 (list.Length + powerToStart) 
+                   loop powerToStart numberOfElementsMinusOne (p::list) p
+            | _ -> let newElement = 2 * acc
+                   loop powerToStart numberOfElementsMinusOne (newElement::list) newElement
+        loop powerToStart numberOfElementsMinusOne [] 2
---
+++ FillListWithPowersOfTwo/fillListWithPowersOfTwo.Tests/fillListWithPowersOfTwoTests.fs
@@ -0,0 +1,25 @@
+﻿module Tests
+
+    open Logic
+    open NUnit.Framework
+    open FsUnit
+
+    [<Test>]
+    let ``Fill list with powers of two from power 3 with 2 elements = [8; 16]``() =
+        fillListWithPowersOfTwo 3 2 |> should equal [8; 16]
+
+    [<Test>]
+    let ``Fill list with powers of two from power 5 with 3 elements = [32; 64; 128]``() =
+        fillListWithPowersOfTwo 5 3 |> should equal [32; 64; 128]
+
+    [<Test>]
+    let ``Fill list with powers of two from power 0 with 3 elements = [1; 2; 4]``() =
+        fillListWithPowersOfTwo 0 3 |> should equal [1; 2; 4]
+
+    [<Test>]
+    let ``Fill list with powers of two from power 1 with 4 elements =  [2; 4; 8; 16]``() =
+        fillListWithPowersOfTwo 1 4 |> should equal [2; 4; 8; 16]
+
+    [<Test>]
+    let ``Fill list with powers of two from power 1 with 1 elements = [2]``() =
+        fillListWithPowersOfTwo 1 1 |> should equal [2]
