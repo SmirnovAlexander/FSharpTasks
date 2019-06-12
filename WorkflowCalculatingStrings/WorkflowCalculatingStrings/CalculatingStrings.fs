1281684e17e6f9d83e27997d9bb7dd2dd27bb5a0
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-06-12 17:45:20 +0300

    Uploaded project back

---
+++ WorkflowCalculatingStrings/WorkflowCalculatingStrings/CalculatingStrings.fs
@@ -0,0 +1,13 @@
+﻿module Logic
+    
+    open System
+
+    // Workflow that make math operations with numbers
+    // given in strings.
+    type StringCalculationBuilder() =
+        member this.Bind(x: string, f) =                         
+            let res = Int32.TryParse(x)
+            if  (fst res) then f (snd res)
+            else None                             
+        member this.Return(x) =
+            Some(x)
