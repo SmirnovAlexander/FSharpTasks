2566c34e68c19900b025cc1f8f47241229a33fba
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-06-12 15:06:44 +0300

    Uploaded project back

---
+++ CheckForPalindrome/CheckForPalindrome/CheckForPalindrome.fs
@@ -0,0 +1,9 @@
+﻿module Logic
+
+    // Reversing string using pipe forwars operator.
+    let reverse (word: string): string = 
+        word.ToCharArray() |> Array.rev |> System.String
+    
+    // Checking for palondrome.
+    let checkForPalindrome word =    
+        word = reverse word
