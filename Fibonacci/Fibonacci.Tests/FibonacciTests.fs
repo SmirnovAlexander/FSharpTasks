4aa16f64e8fb029b703127fa528010033805a7ab
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-06-12 14:02:30 +0300

    Added tests and appveyor.yml

---
+++ Fibonacci/Fibonacci.Tests/FibonacciTests.fs
@@ -0,0 +1,33 @@
+﻿module Tests
+
+    open Logic
+    open NUnit.Framework
+    open FsUnit
+
+    [<Test>]
+    let ``fibonacci 5 = 5``() =
+        fibonacci 5 |> should equal 5
+
+    [<Test>]
+    let ``fibonacci 6 = 8``() =
+        fibonacci 6 |> should equal 8
+
+    [<Test>]
+    let ``fibonacci 0 = 0``() =
+        fibonacci 0 |> should equal 0
+
+    [<Test>]
+    let ``fibonacci 1 = 1``() =
+        fibonacci 1 |> should equal 1
+
+    [<Test>]
+    let ``fibonacci 2 = 1``() =
+        fibonacci 2 |> should equal 1
+
+    [<Test>]
+    let ``fibonacci 3 = 2``() =
+        fibonacci 3 |> should equal 2
+
+    [<Test>]
+    let ``fibonacci -2 -----> WrongInputError``() =
+        (fun () -> fibonacci -2 |> ignore) |> should throw typeof<WrongInputError>
---
+++ Fibonacci/Fibonacci/Fibonacci.fs
@@ -0,0 +1,16 @@
+﻿module Logic
+
+    // Catching input below zero.
+    exception WrongInputError of string
+
+    // Fibonacci recursion function with extra pointer.
+    let fibonacci index =
+        let rec loop counter prev2 prev1 =
+            match counter with
+            | 0 -> prev2
+            | x when x < 0 -> raise (WrongInputError("The given number is below zero."))
+            | _ ->
+                let decreasedCounter = counter - 1
+                let sum = prev2 + prev1
+                loop decreasedCounter prev1 sum
+        loop index 0 1
