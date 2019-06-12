3ad5c36b0137dba7d462aad9e9e2554444b647fb
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-06-12 14:13:33 +0300

    Added tests and appveyor.yml

---
+++ ListReverse/ListReverse/ListReverse.fs
@@ -0,0 +1,9 @@
+﻿module Logic
+
+    // To make it work faster we will use accumulator argument.
+    let reverse list =
+        let rec loop list acc =
+            match list with
+            | [] -> acc
+            | head::tail -> loop tail (head::acc)
+        loop list []
