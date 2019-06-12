88d65fc1e11fc1621df3ac9440b445acbac7b495
SmirnovAlexander <32129186+SmirnovAlexander@users.noreply.github.com>
2019-06-12 15:06:25 +0300

    Deleted project to uppercase it's name

--- checkForPalindrome/checkForPalindrome/checkForPalindrome.fs
+++
@@ -1,9 +0,0 @@
-﻿module Logic
-
-    // Reversing string using pipe forwars operator.
-    let reverse (word: string): string = 
-        word.ToCharArray() |> Array.rev |> System.String
-    
-    // Checking for palondrome.
-    let checkForPalindrome word =    
-        word = reverse word
